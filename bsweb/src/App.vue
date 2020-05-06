3c5484baf90ebb2d3f899a959dae06586f29b574
dsnunes07 <dsnunes@usp.br>
2020-05-06 14:35:14 -0300

    add buefy styles and map component

--- bsweb/src/App.vue
+++ bsweb/src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <div id="app">
-    <filter-panel></filter-panel>
-    <selectors id="selectors"></selectors>
+<div>
+  <div id="header"></div>
+  <div class="container columns">
+    <div class="column is-2 left">
+      <filter-panel></filter-panel>
+    </div>
+    <div class="column is-10 middle">
+      <selectors></selectors>
+      <app-map></app-map>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
   import FilterPanelVue from './components/filters/Filters.vue';
   import SelectorsVue from './components/filters/Selectors.vue';
+  import MapVue from './components/map/Map.vue';
 
   export default {
     components: {
       filterPanel: FilterPanelVue,
-      selectors: SelectorsVue
+      selectors: SelectorsVue,
+      appMap: MapVue
     }
   }
 </script>
 
 <style>
-  html, body {
-    margin: 0;
-    height: 100%;
-    overflow: hidden
+  #header {
+    width: 100%;
+    height: 2vh;
+    background-color: black;
+    display: block;
+  }
+  .container {
+    height: 90vh;
+  }
+
+  .left {
+  }
+
+  .middle {
+    padding: 0;
   }
 </style>
