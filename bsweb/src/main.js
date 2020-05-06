493e7ed6f25226e6a97afec436c4db9252185c89
dsnunes07 <dsnunes@usp.br>
2020-05-06 14:36:27 -0300

    add a eventbus, new imports, buefy and vue resources

--- bsweb/src/main.js
+++ bsweb/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueResource from 'vue-resource'
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
+import 'leaflet/dist/leaflet.css';
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
+import './assets/styles/index.css'
 
+Vue.use(Buefy);
 Vue.use(VueResource);
+
+export const filterEvents = new Vue();
 
 new Vue({
   el: '#app',
