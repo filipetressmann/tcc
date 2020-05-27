06acfb11a7f487bff29c0c00c3517fa89d3b0727
dsnunes07 <dsnunes@usp.br>
2020-05-27 12:02:21 -0300

    File to serve layers data, eg cptm subway and bike lanes

---
+++ server/layers.py
@@ -0,0 +1,37 @@
+import geopandas as gpd
+from flask_restful import Resource
+from os.path import abspath
+
+data_path = abspath('./') + '/data'
+
+# provides CPTM railway layer
+class CPTM(Resource):
+  def load_railway(self):
+    railway = gpd.GeoDataFrame.from_file(data_path + "/shapes//SIRGAS_SHP_linhatrem_line.shp", encoding='latin-1')
+    railway.crs = {'init' :'epsg:22523'}
+    railway = railway.to_crs({"init": "epsg:4326"})
+    return railway
+
+  def get(self):
+    return self.load_railway().to_json()
+
+# provides Metro layer
+class Metro(Resource):
+  def load_metro(self):
+    metro = gpd.GeoDataFrame.from_file(data_path + "/shapes//SIRGAS_SHP_linhametro_line.shp", encoding='latin-1')
+    metro.crs = {'init': 'epsg:22523'}
+    metro = metro.to_crs({'init': 'epsg:4326'})
+    return metro
+
+  def get(self):
+    return self.load_metro().to_json()
+
+class BikeLane(Resource):
+  def load_bike_lane(self):
+    bikelane = gpd.GeoDataFrame.from_file(data_path + "/shapes//SIRGAS_SHP_redecicloviaria.shp", encoding='latin-1')
+    bikelane.crs = {'init': 'epsg:22523'}
+    bikelane = bikelane.to_crs({'init': 'epsg:4326'})
+    return bikelane
+    
+  def get(self):
+    return self.load_bike_lane().to_json()
