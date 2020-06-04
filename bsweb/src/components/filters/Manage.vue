3a8f4f48ab39293baeae1900af1c4f0c88dd257d
dsnunes07 <dsnunes@usp.br>
2020-06-04 17:18:53 -0300

    creata tabs to manage data, maps and filters

---
+++ bsweb/src/components/filters/Manage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container" id="filter-container">
+    <p id="title">Bike Science Web</p>
+    <b-tabs position="is-centered" v-model="activeTab">
+      <b-tab-item>
+        <template slot="header">
+          <span>Dados</span>
+        </template>
+        Tiers; Source; Import; Export;
+      </b-tab-item>
+      <b-tab-item>
+        <template slot="header">
+          <span>Filtros <b-tag rounded>{{ activeFilters.length }}</b-tag></span>
+        </template>
+        <filters />
+      </b-tab-item>
+      <b-tab-item>
+        <template slot="header">
+          <span>Mapas <b-tag rounded>1</b-tag></span>
+        </template>
+        Select maps to filter; Select maps that will be rendered;
+      </b-tab-item>
+    </b-tabs>
+    
+  </div>
+</template>
+
+<script>
+  import { filterEvents } from '../../main.js';
+  import { mapActions } from 'vuex';
+  import Filter from './Filter';
+  import FiltersVue from './Filters.vue';
+
+  export default {
+    components: {
+      'app-filter': Filter,
+      'filters': FiltersVue
+    },
+    data() {
+      return {
+        isOpen: -1,
+        filters: [],
+        activeTab: 0
+      };
+    },
+    computed: {
+      activeFilters() {
+        return this.$store.getters.activeFilters;
+      }
+    }
+  }
+</script>
