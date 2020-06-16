0aaee4c0cfdb7dfc53966eefee4be16f0e31ee05
dsnunes07 <dsnunes@usp.br>
2020-06-16 16:07:04 -0300

    Use vuei18n in Vue instance

--- bsweb/src/main.js
+++ bsweb/src/main.js
@@ -1,18 +1,25 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueResource from 'vue-resource';
-import 'leaflet/dist/leaflet.css';
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import 'leaflet/dist/leaflet.css'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
 import './assets/styles/index.css'
-import { store } from './store/store.js';
+import VueI18n from 'vue-i18n'
+import { messages } from './plugins/i18n'
+import { store } from './store/store.js'
 
+Vue.use(VueI18n);
 Vue.use(Buefy);
 Vue.use(VueResource);
+
+
 export const filterEvents = new Vue();
+export const i18n = new VueI18n({ locale: "pt-br", fallbackLocale: "en", messages });
 
 new Vue({
+  i18n,
   el: '#app',
   store,
   render: h => h(App)
