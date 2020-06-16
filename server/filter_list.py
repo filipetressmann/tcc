5f551037d6a3f52d75f5f2d2028b23a558ab4112
dsnunes07 <dsnunes@usp.br>
2020-06-16 16:07:49 -0300

    Change filter names to be translated

--- server/filter_list.py
+++ server/filter_list.py
@@ -1,145 +1,131 @@
 # This object configures the filter list on view app. Also manages the creation of filter forms.
 FILTERS = [
   {
-    'category_name': 'Layers',
+    'category_name': 'layers',
     'filters': [
       {
         'id': 14,
         'filter_key': 'cptm',
         'filter_type': 'geojson',
-        'filter_name': 'Linhas da CPTM',
+        'filter_name': 'spRailway',
         'has_form': False
       },
       {
         'id': 15,
         'filter_key': 'subway',
         'filter_type': 'geojson',
-        'filter_name': 'Linhas do Metrô',
+        'filter_name': 'spSubway',
         'has_form': False
       },
       {
         'id': 16,
         'filter_key': 'bikelane',
         'filter_type': 'geojson',
-        'filter_name': 'Malha cicloviária',
+        'filter_name': 'spBikelanes',
         'has_form': False
       },
     ]
   },
   {
-    'category_name': 'Filtros temporais',
+    'category_name': 'timeFilters',
     'filters': [
       {
       'id': 0,
       'filter_key': 'horary',
       'filter_type': 'polyline',
-      'filter_name': 'Horário',
+      'filter_name': 'horary',
       'has_form': True
       },
       {
         'id': 1,
         'filter_key': 'weekday',
         'filter_type': 'polyline',
-        'filter_name': 'Dia da semana',
+        'filter_name': 'weekday',
         'has_form': True
       },
       {
         'id': 2,
         'filter_key': 'holidays',
         'filter_type': 'polyline',
-        'filter_name': 'Feriados' ,
+        'filter_name': 'holidays' ,
         'has_form': True
       }
     ]
   },
   {
-    'category_name': 'Filtros de viagem',
+    'category_name': 'tripFilters',
     'filters': [
       {
         'id': 3,
         'filter_key': 'distance',
         'filter_type': 'todo',
-        'filter_name': 'Distância percorrida',
+        'filter_name': 'tripDistance',
         'has_form': False
       },
       {
         'id': 4,
         'filter_key': 'speed',
         'filter_type': 'todo',
-        'filter_name': 'Velocidade média',
+        'filter_name': 'avgSpeed',
         'has_form': False
       },
       {
         'id': 5,
         'filter_key': 'duration',
         'filter_type': 'todo',
-        'filter_name': 'Duração',
+        'filter_name': 'tripDuration',
         'has_form': False
       },
       {
         'id': 6,
         'filter_key': 'reason',
         'filter_type': 'todo',
-        'filter_name': 'Motivo da viagem',
+        'filter_name': 'tripReason',
         'has_form': False
       }
     ]
   },
   {
-    'category_name': 'Filtros demográficos',
+    'category_name': 'populationFilters',
     'filters': [
       {
         'id': 7,
         'filter_key': 'gender',
         'filter_type': 'todo',
-        'filter_name': 'Gênero',
+        'filter_name': 'gender',
          'has_form': False
       },
       {
         'id': 8,
         'filter_key': 'age',
         'filter_type': 'polyline',
-        'filter_name': 'Idade',
+        'filter_name': 'age',
         'has_form': True
       },
       {
         'id': 9,
         'filter_key': 'income',
         'filter_type': 'todo',
-        'filter_name': 'Faixa de renda',
+        'filter_name': 'income',
         'has_form': False
       },
-      {
-        'id': 10,
-        'filter_key': 'bikereason',
-        'filter_type': 'todo',
-        'filter_name': 'Razões da escolha por bicicleta',
-        'has_form': False
-      },
-      {
-        'id': 11,
-        'filter_key': 'walkreason',
-        'filter_type': 'todo',
-        'filter_name': 'Razões da escolha por andar a pé',
-        'has_form': False
-      }
     ]
   },
   {
-    'category_name': 'Filtros geográficos',
+    'category_name': 'geographicFilters',
     'filters': [
       {
         'id': 12,
         'filter_key': 'attractor',
         'filter_type': 'todo',
-        'filter_name': 'Regiões atratoras de viagens',
+        'filter_name': 'attractor',
         'has_form': False
       },
       {
         'id': 13,
         'filter_key': 'emitter',
         'filter_type': 'todo',
-        'filter_name': 'Regiões emissoras de viagens',
+        'filter_name': 'emitter',
         'has_form': False
       }
     ]
