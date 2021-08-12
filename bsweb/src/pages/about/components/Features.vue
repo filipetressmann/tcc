468d1c9b6491944f0b0927cb29bbb8c0182c3266
Thiago Pena <thiago.pena@usp.br>
2021-08-11 21:57:41 -0300

    Fixed some texts on about page

--- bsweb/src/pages/about/components/Features.vue
+++ bsweb/src/pages/about/components/Features.vue
@@ -4,10 +4,10 @@
     <p>As funcionalidades da ferramenta estão organizadas em três grupos:</p>
     <ol>
       <li>
-        <strong>Fluxos:</strong> contém as opções para definir e ajustar a grade na qual os fluxos serão computados. Os fluxos são organizados em 4 quartis; cada um deles responsável por 25% do total de viagens. O primeiro quartil contém os fluxos mais intensos, são poucos fluxos mas cada um contendo muitas viagens, e assim por diante até o último quartil, com muitos fluxos, mas cada um com poucas viagens.
+        <strong>Fluxos:</strong> contém as opções para definir e ajustar a grade na qual os fluxos serão computados. Os fluxos são organizados em 4 quartis, cada um deles responsável por 25% do total de viagens. O primeiro quartil contém os fluxos mais intensos, ou seja, poucos fluxos mas cada um contendo muitas viagens, e assim por diante até o último quartil, que tem muitos fluxos, mas cada um com poucas viagens.
       </li>
       <li>
-        <strong>Filtros:</strong> contém opções de filtros temporais, de condições de viagens e demográficos a serem aplicados nos fluxos. Ao selecionar uma categoria de filtros aparecem suas opções, que quando ativadas filtram automaticamente os fluxos.
+        <strong>Filtros:</strong> contém opções de filtros temporais, de condições de viagens e demográficos a serem aplicados nos fluxos. Ao selecionar uma categoria de filtros aparecem suas opções, que quando ativadas, filtram automaticamente os fluxos.
       </li>
       <li>
         <strong>Camadas:</strong> contém opções de visualização de camadas de infraestrutura de transporte, cicloviária e de ocorrências de trânsito.
